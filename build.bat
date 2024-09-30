92e4f9d0b58dd469c68b9e63bc63b94d79016077
oscarg111 <oscaresteban04@gmail.com>
2024-09-30 15:56:59 -0400

    Your commit message here

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-set files=src\glad.c src\main.c
+set files=src\glad.c src\main.c src\engine\global.c src\engine\render\render.c src\engine\render\render_init.c
 set libs=Q:\lib\SDL2main.lib Q:\lib\SDL2.lib Q:\lib\freetype.lib
 
 CL /Zi /I Q:\include %files% /link %libs% /OUT:mygame.exe
