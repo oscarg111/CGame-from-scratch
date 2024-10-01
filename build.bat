302cdeb75301eeb7ddd1977f3e32a34407841592
oscarg111 <oscaresteban04@gmail.com>
2024-09-30 23:47:29 -0400

    Your commit message here

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
-set files=src\glad.c src\main.c src\engine\global.c src\engine\render\render.c src\engine\render\render_init.c
-set libs=Q:\lib\SDL2main.lib Q:\lib\SDL2.lib Q:\lib\freetype.lib
+set render=src\engine\render\render.c src\engine\render\render_init.c src\engine\render\render_util.c
+set io=src\engine\io\io.c
+set files=src\glad.c src\main.c src\engine\global.c %render% %io%
+set libs=C:\work\lib\SDL2main.lib C:\work\lib\SDL2.lib C:\work\lib\freetype.lib
 
-CL /Zi /I Q:\include %files% /link %libs% /OUT:mygame.exe
+CL /Zi /I C:\work\include %files% /link %libs% /OUT:mygame.exe
